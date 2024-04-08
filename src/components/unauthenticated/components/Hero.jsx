14e384115d0e70660fa77a04103b964477f7d1a0
Santhosh <shanmugamsanthosh22@gmail.com>
2024-04-08 15:02:02 +0530

    api change

--- src/components/unauthenticated/components/Hero.jsx
+++ src/components/unauthenticated/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <section className="text-gray-600 body-font">
       <div className="w-full h-[80vh] mx-auto flex lg:px-24 px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="lg:flex-grow md:w-1/2 mt-10 md:mt-0 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 leading-tight">Stay Updated with the Latest News
             <br className="hidden lg:inline-block"/>from Around the World
           </h1>
